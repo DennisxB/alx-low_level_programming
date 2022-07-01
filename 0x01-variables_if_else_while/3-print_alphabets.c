d7a04fa936dd3c008aa4030b7e8ef5f1523a6f86
Dennis Botwe <donc00930@gmail>
2022-07-01 04:01:52 -0700

    prints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,29 @@
+
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - alphabet in lowercase, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Always return(0)
  */
+
 int main(void)
 {
-int x, y;
+	char a = 'a';
+	char b = 'A';
 
-<<<<<<< HEAD
-for (m = 97; m <= 122; m++)
-putchar(m);
-for (n = 65; n <= 90; n++)
-putchar(n);
-=======
-for (x = 97; x<= 122; x++)
-{
-putchar(x);
-}
-for (y = 65; y <= 90; y++)
-{
-putchar(y);
-}
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-putchar('\n');
-return (0);
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+
+	return (0);
+
 }
